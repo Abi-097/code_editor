1183880c79a60347760f8d6b158292c1dfbac8be
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-06-10 22:32:16 +0530

    home page layout added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,59 @@
 import React from "react";
-
+import Logo from "../assets/Logo.png";
 const Home = () => {
   return (
     <div className="container-fluid">
       <div className="row justify-content-center align-items-center min-vh-100">
-        <div className="col-12 col-md-6"></div>
+        <div className="col-12 col-md-6">
+          <div className="card shadow-sm p-2 mb-5 bg-secondary rounded">
+            <div className="card-body text-center bg-dark">
+              <img
+                src={Logo}
+                alt="Logo"
+                className="img-fluid mx-auto d-block"
+                style={{ maxWidth: "150px" }}
+              />
+              <h4 className="card-title text-light mb-4">Enter the ROOM ID</h4>
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  //   value={roomId}
+                  //   onChange={(e) => setRoomId(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="ROOM ID"
+                  //   onKeyUp={handleInputEnter}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  //   value={username}
+                  //   onChange={(e) => setUsername(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="USERNAME"
+                  //   onKeyUp={handleInputEnter}
+                />
+              </div>
+              <button
+                // onClick={joinRoom}
+                className="btn btn-success btn-lg btn-block"
+              >
+                JOIN
+              </button>
+              <p className="mt-3 text-light">
+                Don&apos;t have a room ID? create
+                <span
+                  //   onClick={generateRoomId}
+                  className=" text-success p-1"
+                  style={{ cursor: "pointer" }}
+                >
+                  New Room
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
